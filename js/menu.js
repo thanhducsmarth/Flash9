a1124e0708c748a1d79706c28f132df865ccfde8
Tran Thanh Duc <thanhduckrb767@gmail.com>
2023-01-03 12:22:27 +0700

    Flash9 init

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if(navToggle)
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+});
+
+if(navClose)
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+});
+
+
+const ActivePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a');
+forEach(link => {
+    if(link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+    }
+});
